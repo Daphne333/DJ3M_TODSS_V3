de10ffaae8300ba4d3b2242590d863f05ffbc702
jbsleepz <kotshond@gmail.com>
2017-05-03 15:34:09 +0200

    PersoonDAO volgens hibernate.

--- src/dao/PersoonDAO.java
+++ src/dao/PersoonDAO.java
@@ -3,26 +3,93 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
+import domein.Bedrijf;
 import domeinBackup.Persoon;
+import util.HibernateUtil;
 public class PersoonDAO {
-
-	public void create(Persoon persoon) {
-		try {
-			MysqlDataSource dataSource = new MysqlDataSource();
-			dataSource.setUser("root");
-			dataSource.setPassword("joukebouwe");
-			dataSource.setURL("jdbc:mysql://localhost:3306/worlddb");
-
-			Connection conn = dataSource.getConnection();
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT 5 FROM DUAL");
-			stmt.close();
-			conn.close();} catch(SQLException sqle){sqle.printStackTrace();}
-			
-	}
+	
+	Session session = HibernateUtil.getSessionFactory().openSession();
+	Transaction connection = null;
 	
 
+	public void create(Persoon persoon) {
+		
+		try {
+			connection = session.beginTransaction();
+			session.save(persoon);
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			if(connection != null){
+				connection.rollback();
+			} else {
+				e.printStackTrace();
+			}
+		} finally {
+			session.flush();
+			session.close();
+		}
+	}
+	
+	public void update(Persoon persoon){
+		
+		try {
+			connection = session.beginTransaction();
+			session.update(persoon);
+			session.getTransaction();
+		} catch (RuntimeException e){
+			if(connection != null){
+				connection.rollback();
+			} else {
+				e.printStackTrace();
+			}
+		} finally {
+			session.flush();
+			session.close();
+		}
+		
+	}
+	
+	public Persoon getPersoonByID(int persoonID){
+		Persoon persoon = null;
+		session = HibernateUtil.getSessionFactory().openSession();
+		try{
+			connection = session.beginTransaction();
+			String queryString = "from Persoon where personID = :personID";
+			Query query = session.createQuery(queryString);
+			query.setInteger("personID", persoonID);
+			persoon = (Persoon)query.uniqueResult();
+		} catch(RuntimeException e){
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		}
+		return persoon;
+	}	
+	
+	public List<Persoon> getPersonen() {
+		List<Persoon> personen = new ArrayList<Persoon>();
+		session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			connection = session.beginTransaction();
+			personen = session.createQuery("From Persoon").list();
+		} catch(RuntimeException e){
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		}
+		return personen;
+	}
+
+	
 }
