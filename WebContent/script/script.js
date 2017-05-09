4afb2f7095a5be55b010fbfbb37f98125990c58e
jellyforce <jelly.wie@gmail.com>
2017-05-09 15:04:58 +0200

    pagina's bijwerken

--- WebContent/script/script.js
+++ WebContent/script/script.js
@@ -1,25 +1,27 @@
+/*
 
-/*als eerst gaan we de header/footer toevoegen op de pagina*/
+Als de pagina wordt gestart doe dit:
+als eerst gaan we de header/footer toevoegen op de pagina*/
 $(document).ready(function(){
 	 
-	  $('#header').load("/standard/header.html"); 
+	  $('#header').load("../standard/header.html"); 
 /*	  $("#footer").load("../standard/footer.html"); */
 
 	  var rol = null
 	  
 	  if (document.getElementById('rol') == null || documenbt.getElementById('rol') == ''){
-		  alert('er moet iemand ingelogd zijn');
+		  alert('er moet iemand ingelogd zijn om een men te tonen!');
 	  }
 	  else{
 
 		  //afhankelijk van jouw rol krijg je de desbetreffende menu-bar te zien
 		  switch(rol){
 			  case "docent":
-				  $('#menu').load("../standand/bedrijf_menu.html");
+				  $('#menu').load("../standard/bedrijf_menu.html");
 			  case "cursist":
-				  $('#menu').load("../standand/cursist_menu.html");
+				  $('#menu').load("../standard/cursist_menu.html");
 			  case " manager":
-				  $('#menu').load("../standand/manager_menu.html");
+				  $('#menu').load("../standard/manager_menu.html");
 			  case "beheerder":
 				  $('#men').load("../standard/beheerder_menu.html");
 		  }
