f4b5716d1ad3ff09b728405e658fd0c88aebdc9a
JoeydeRijk <joey.derijk@student.hu.nl>
2017-05-18 14:46:44 +0200

    Account merge

--- src/Model/domein/Account.java
+++ src/Model/domein/Account.java
@@ -1,13 +1,18 @@
 package Model.domein;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -20,10 +25,13 @@ public class Account {
 	private String username, password;
 	private Persoon persoon;
 
-	//CURSIST,MANAGER,BEHEERDER,DOCENT 
+	//CURSIST,MANAGER,BEHEERDER,DOCENT
 	private FunctieRol rol;
 	private byte[] salt;
 	private int accountID;
+	private Set<CursusUitvoering> uitvoeringen = new HashSet<CursusUitvoering>();
+	private Set<CursusUitvoering> geeftCursus = new HashSet<CursusUitvoering>();
+
 	public Account(){
 		
 	}
@@ -107,5 +115,23 @@ public class Account {
 	public void setSalt(byte[] salt) {
 		this.salt = salt;
 	}
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "cursisten")
+	public Set<CursusUitvoering> getUitvoeringen() {
+		return this.uitvoeringen;
+	}
+
+	public void setUitvoeringen(Set<CursusUitvoering> uitvoeringen) {
+		this.uitvoeringen = uitvoeringen;
+	}
+
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "docenten")
+	public Set<CursusUitvoering> getGeeftCursus() {
+		return this.geeftCursus;
+	}
+
+	public void setgeeftCursus(Set<CursusUitvoering> geeftCursus) {
+		this.geeftCursus = geeftCursus;
+	}
+
 
 }
