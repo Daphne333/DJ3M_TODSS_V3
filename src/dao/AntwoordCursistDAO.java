f24e48433d51f2251755f89fe40d7db8d223af0b
Daphne Hogeterp <daphne_hogeterp@hotmail.com>
2017-05-18 16:32:09 +0200

    AntwoordCursistDAO gemaakt + deel AntwoordOpdrachtDAO

--- src/dao/AntwoordCursistDAO.java
+++ src/dao/AntwoordCursistDAO.java
@@ -1,5 +1,88 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import Model.domein.AntwoordCursist;
+import util.HibernateUtil;
+
 public class AntwoordCursistDAO {
+	
+	Session session = HibernateUtil.getSessionFactory().openSession();
+	Transaction connection = null;
+
+	public void create(AntwoordCursist antwoordCursist) {
+		try {
+			connection = session.beginTransaction();
+			session.save(antwoordCursist);
+			session.getTransaction().commit();
+		} catch (RuntimeException e) {
+			if (connection != null) {
+				connection.rollback();
+			} else {
+				e.printStackTrace();
+			}
+		} finally {
+			session.flush();
+			session.close();
+		}
+	}
+
+	public void update(AntwoordCursist antwoordCursist) {
+
+		try {
+			connection = session.beginTransaction();
+			session.update(antwoordCursist);
+			session.getTransaction();
+		} catch (RuntimeException e) {
+			if (connection != null) {
+				connection.rollback();
+			} else {
+				e.printStackTrace();
+			}
+		} finally {
+			session.flush();
+			session.close();
+		}
+
+	}
+
+	public AntwoordCursist getAntwoordCursistByID(int antwoordCursistID) {
+		AntwoordCursist antwoordCursist = null;
+		session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			connection = session.beginTransaction();
+			String queryString = "from Antwoord_Cursist where antwoordCursistID = :antwoordCursistID";
+			Query query = session.createQuery(queryString);
+			query.setInteger("antwoordCursistID", antwoordCursistID);
+			antwoordCursist = (AntwoordCursist) query.uniqueResult();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		}
+		return antwoordCursist;
+	}
+
+	public List<AntwoordCursist> getAntwoordCursist() {
+		List<AntwoordCursist> antwoordCursist = new ArrayList<AntwoordCursist>();
+		session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			connection = session.beginTransaction();
+			antwoordCursist = session.createQuery("From Antwoord_Cursist").list();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		}
+		return antwoordCursist;
+	}
+
 
 }
--- src/dao/AntwoordOpdrachtDAO.java
+++ src/dao/AntwoordOpdrachtDAO.java
@@ -1,5 +1,15 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import Model.domein.AntwoordOpdracht;
+import util.HibernateUtil;
+
 public class AntwoordOpdrachtDAO {
 
 }
