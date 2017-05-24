d238d971f307bcc447113687d41e7c054d54a1a3
jellyforce <jelly.wie@gmail.com>
2017-05-24 16:42:28 +0200

    Cursisten zouden nu toegevoegd moeten kunnen worden in de database.
    Joey, ga jij hier nog even naar kijken of dit zi klopt?

--- src/Controller/Servlets/CursistToevoegenServlet.java
+++ src/Controller/Servlets/CursistToevoegenServlet.java
@@ -1,6 +1,8 @@
 package Controller.Servlets;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -12,8 +14,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import Model.Services.Account.WijzigPersoonService;
+import Model.Services.Account.AccountService;
+import Model.Services.Persoon.PersoonService;
+import Model.Services.Provider.ServiceProvider;
 import Model.domein.Bedrijf;
+import Model.domein.FunctieRol;
 import Model.domein.Persoon;
 
 public class CursistToevoegenServlet extends HttpServlet{
@@ -44,22 +49,24 @@ public class CursistToevoegenServlet extends HttpServlet{
 			//ophalen datum en converten naar date-format
 			String geboortedatum = req.getParameter("geboortedatum");
 			DateFormat sourceFormat = new SimpleDateFormat("dd-MM-yyyy");
+			Date date = null;
 			try {
-				Date date = sourceFormat.parse(geboortedatum);
+				date = sourceFormat.parse(geboortedatum);
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
 			String email = req.getParameter("email");
-			int telefoonnummer = Integer.parseInt(req.getParameter("telefoonnummer"));
+			String telefoonnummer = req.getParameter("telefoonnummer");
+/*			int telefoonnummer = Integer.parseInt(req.getParameter("telefoonnummer"));*/
 			
 			String straatnaam = req.getParameter("straatnaam");
 			int huisnummer = Integer.parseInt(req.getParameter("huisnummer"));
 			String toevoeging = req.getParameter("toevoeging");
 			String postcode = req.getParameter("postcode");
 			String plaats = req.getParameter("plaats");
-			String bsn = req.getParameter("bsn");
+			int bsn = Integer.parseInt(req.getParameter("bsn"));
 			int bedrijfsID = Integer.parseInt(req.getParameter("bedrijfID"));
 			String geslacht = req.getParameter("geslacht");
 			int chefID = Integer.parseInt(req.getParameter("chefID"));
@@ -90,14 +97,41 @@ public class CursistToevoegenServlet extends HttpServlet{
 			// 						ServiceProvider + DAO + Persoon-Object
 			//====================================================================
 						
-			WijzigPersoonService wps = new WijzigPersoonService(); //DAO zit hierin
+			ServiceProvider sp = new ServiceProvider();
 			
-			Persoon p = new Persoon email,  password,  naam,  tussenvoegsel,  achternaam,
-					 geboortedatum,  telefoonnummer,  straatnaam,  huisnummer,  toevoeging,
-					 postcode,  plaats,  bsnnummer,  geslacht,  bedrijfID);
+			Bedrijf bedrijfID = new Bedrijf();
+			String password = "";
+			
+			Persoon p = new Persoon (email,  password,  naam,  tussenvoegsel,  achternaam,
+					date,  telefoonnummer,  straatnaam,  huisnummer,  toevoeging,
+					 postcode,  plaats,  bsn,  geslacht,  bedrijfID);
 			
 			
 			
+			
+			
+			
+			@SuppressWarnings("static-access")
+			AccountService ac = sp.getAccountService();
+			try {
+				ac.Register(p, naam + "01", password, FunctieRol.CURSIST);                // wachtwoord generator???
+			} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		
 			
 			System.out.println("CursistToevoegenServlet");
 			String url = null; //hier gaan we naartoe verwijzen;
