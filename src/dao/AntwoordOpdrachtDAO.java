d3caa65b86be08231e76af828d0fa2c128e3c751
jbsleepz <kotshond@gmail.com>
2017-05-24 15:00:23 +0200

    Alle DAO"S  werken weer ??????

--- src/dao/AntwoordOpdrachtDAO.java
+++ src/dao/AntwoordOpdrachtDAO.java
@@ -11,80 +11,45 @@ import Model.domein.AntwoordOpdracht;
 import util.HibernateUtil;
 
 public class AntwoordOpdrachtDAO {
-	
-	
+
 	Session session = HibernateUtil.getSessionFactory().openSession();
 	Transaction connection = null;
 
 	public void create(AntwoordOpdracht antwoordOpdracht) {
-		try {
-			connection = session.beginTransaction();
-			session.save(antwoordOpdracht);
-			session.getTransaction().commit();
-		} catch (RuntimeException e) {
-			if (connection != null) {
-				connection.rollback();
-			} else {
-				e.printStackTrace();
-			}
-		} finally {
-			session.flush();
-			session.close();
-		}
+
+		connection = session.beginTransaction();
+		session.save(antwoordOpdracht);
+		session.getTransaction().commit();
 	}
 
 	public void update(AntwoordOpdracht antwoordOpdracht) {
 
-		try {
-			connection = session.beginTransaction();
-			session.update(antwoordOpdracht);
-			session.getTransaction();
-		} catch (RuntimeException e) {
-			if (connection != null) {
-				connection.rollback();
-			} else {
-				e.printStackTrace();
-			}
-		} finally {
-			session.flush();
-			session.close();
-		}
-
+		connection = session.beginTransaction();
+		session.update(antwoordOpdracht);
+		session.getTransaction();
 	}
 
 	public AntwoordOpdracht getAntwoordOpdrachtByID(int antwoordOpdrachtID) {
 		AntwoordOpdracht antwoordOpdracht = null;
 		session = HibernateUtil.getSessionFactory().openSession();
-		try {
-			connection = session.beginTransaction();
-			String queryString = "from Antwoord_Opdracht where antwoordOpdrachtID = :antwoordOpdrachtID";
-			Query query = session.createQuery(queryString);
-			query.setInteger("antwoordOpdrachtID", antwoordOpdrachtID);
-			antwoordOpdracht = (AntwoordOpdracht) query.uniqueResult();
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		} finally {
-			session.flush();
-			session.close();
-		}
+
+		connection = session.beginTransaction();
+		String queryString = "from Antwoord_Opdracht where antwoordOpdrachtID = :antwoordOpdrachtID";
+		Query query = session.createQuery(queryString);
+		query.setInteger("antwoordOpdrachtID", antwoordOpdrachtID);
+		antwoordOpdracht = (AntwoordOpdracht) query.uniqueResult();
+
 		return antwoordOpdracht;
 	}
 
-	public List<AntwoordOpdracht> getAntwoordCursist() {
+	public List<AntwoordOpdracht> getListAntwoordCursisten() {
 		List<AntwoordOpdracht> antwoordOpdracht = new ArrayList<AntwoordOpdracht>();
 		session = HibernateUtil.getSessionFactory().openSession();
-		try {
-			connection = session.beginTransaction();
-			antwoordOpdracht = session.createQuery("From Antwoord_Opdracht").list();
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		} finally {
-			session.flush();
-			session.close();
-		}
+
+		connection = session.beginTransaction();
+		antwoordOpdracht = session.createQuery("From Antwoord_Opdracht").list();
+
 		return antwoordOpdracht;
 	}
 
-
 }
-
--- src/dao/CursistUitvoering.java
+++ src/dao/CursistUitvoering.java
@@ -1,5 +1,57 @@
 package dao;
 
-public class CursistUitvoering {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import Model.domein.Cursus;
+import Model.domein.CursusUitvoering;
+import util.HibernateUtil;
+
+public class CursistUitvoering {
+	
+	Session session = HibernateUtil.getSessionFactory().openSession();
+	Transaction connection = null;
+
+	public void create(CursusUitvoering uitvoering) {
+
+		connection = session.beginTransaction();
+		session.save(uitvoering);
+		session.getTransaction().commit();
+
+	}
+
+	public void update(CursusUitvoering uitvoering) {
+
+		connection = session.beginTransaction();
+		session.update(uitvoering);
+		session.getTransaction();
+
+	}
+
+	public CursusUitvoering getCursusUitvoeringByID(int uitvoeringID) {
+		CursusUitvoering uitvoering = null;
+		session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+		connection = session.beginTransaction();
+		String queryString = "from Cursus_uitvoering where uitvoeringID = :uitvoeringID";
+		Query query = session.createQuery(queryString);
+		query.setInteger("uitvoeringID", uitvoeringID);
+		uitvoering = (CursusUitvoering) query.uniqueResult();
+
+		return uitvoering;
+	}
+
+	public List<CursusUitvoering> getListCursusUitvoeringen() {
+		List<CursusUitvoering> alleUitvoeringen = new ArrayList<CursusUitvoering>();
+		session = HibernateUtil.getSessionFactory().openSession();
+
+		connection = session.beginTransaction();
+		alleUitvoeringen = session.createQuery("From Cursus").list();
+
+		return alleUitvoeringen;
+	}
 }
