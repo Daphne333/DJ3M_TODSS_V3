e2f41bbb502d374c6e2f1333ec406fe403aef022
jbsleepz <kotshond@gmail.com>
2017-06-13 11:17:46 +0200

    Session attribute

--- WebContent/Cursist/Cursist_Home.jsp
+++ WebContent/Cursist/Cursist_Home.jsp
@@ -1,9 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
     
+<%@ page import="Model.domein.Account"%>
+<%@ page import="Model.domein.FunctieRol"%>
 <!-- de C-prefix is nodig voor de tabel die hieronder wordt gegenereerd. 
      C kan elk ander willekeurig iets zijn -->
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@page language="java" session="true" %>
     
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -17,15 +20,26 @@
 </head>
 <body>
 		
-		<h1>Test hallo Cursist</h1>
-		
+	
+		<td> test</td>
 		<div id="content" class="content">
 		
 		<div class="inner-content"	style="background-color: white; opacity: .9;">
 <!-- 		in de messagebox wordt de data opgevangen die wij terugkrijgen van de servlets/dao/database -->
 		<div id="messagebox">
 		<%
+			
+			Account account = (Account) session.getAttribute("loginAccount"); 
+			//request.getSession().getAttribute("loginAccount");    
+			
+			//FunctieRol rol = account.getRol();
+    		//String naam = account.getUsername();
 		
+		%>
+		<td>Username: </td>
+		<td><input type="text" value="<%= account.getUsername()%>" /></td>
+		<%
+			
 			Object msg = request.getAttribute("msg");
 		
 			// dit object moet de informatie bevatten over de gehele planning
--- src/Controller/Servlets/LoginServlet.java
+++ src/Controller/Servlets/LoginServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import Model.Service.AccountService;
 import Model.Service.PersoonService;
@@ -44,19 +45,23 @@ public class LoginServlet extends HttpServlet {
 			//if ((account =(Account) request.getAttribute("loginAccount")) != null) {
 				FunctieRol functieRol = account.getRol();
 				personalInfo = personalService.getPersoonBijAccount(account);
-				request.getSession().invalidate();
-				request.getSession().setAttribute("loginAccount", account);
-				request.getSession().setAttribute("personalInfo", personalInfo);
+				System.out.println(account.getUsername());
+				System.out.println(account.getRol());
+				HttpSession session = request.getSession();
+				//session.invalidate();
+				session.setAttribute("loginAccount", account);
+				session.setAttribute("personalInfo", personalInfo);
+				
 				if (functieRol == FunctieRol.BEHEERDER) {
 					url = "/Beheerder/Beheerder_Home.jsp";
 				} else if (functieRol == FunctieRol.CURSIST) {
 					url = "/Cursist/Cursist_Home.jsp";
+					System.out.print("Hier is het werkende object uit de sessie :" + session.getAttribute("loginAccount").toString());
 				} else if (functieRol == FunctieRol.MANAGER) {
 					url = "/Manager/Manager_Home.jsp";
 				} else if (functieRol == FunctieRol.DOCENT) {
 					url = "/Docent/Docent_Home.jsp";
 				}
-			
 			} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
