0307ad730f7b290d021062106aafb9f0869f13d9
jbsleepz <kotshond@gmail.com>
2017-06-18 01:10:03 +0200

    Methode getFunctieRol toegevoegd.

--- src/Model/domein/FunctieRol.java
+++ src/Model/domein/FunctieRol.java
@@ -1,27 +1,28 @@
 package Model.domein;
 
 public enum FunctieRol {
-	CURSIST,MANAGER,BEHEERDER,DOCENT;
-	
-	public FunctieRol getFunctieRolByName(String functieRoleName){
+	CURSIST, MANAGER, BEHEERDER, DOCENT;
+
+	public FunctieRol getFunctieRolByName(String functieRoleName) {
 		for (FunctieRol functieRol : FunctieRol.values())
-			if(functieRol.name().equalsIgnoreCase(functieRoleName))
+			if (functieRol.name().equalsIgnoreCase(functieRoleName))
 				return functieRol;
 		return null;
 	}
-	public FunctieRol getFunctieRole(String functie){
+
+	public FunctieRol getFunctieRole(String functie) {
 		FunctieRol rol = null;
-		if(functie.equals("")){
-			if(functie.equalsIgnoreCase("CURSIST")){
-				rol = FunctieRol.CURSIST;
-			} else if (functie.equalsIgnoreCase("DOCENT")){
-				rol = FunctieRol.DOCENT;
-			} else if (functie.equalsIgnoreCase("BEHEERDER")){
-				rol = FunctieRol.BEHEERDER;
-			} else if (functie.equalsIgnoreCase("MANAGER")){
-				rol = FunctieRol.MANAGER;
-			}
-		} 
+
+		if (functie.equalsIgnoreCase("CURSIST")) {
+			rol = FunctieRol.CURSIST;
+		} else if (functie.equalsIgnoreCase("DOCENT")) {
+			rol = FunctieRol.DOCENT;
+		} else if (functie.equalsIgnoreCase("BEHEERDER")) {
+			rol = FunctieRol.BEHEERDER;
+		} else if (functie.equalsIgnoreCase("MANAGER")) {
+			rol = FunctieRol.MANAGER;
+		}
+
 		return rol;
 	}
 }
